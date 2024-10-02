6543e8b18aced843ad2c7ec2bb6385b65dc0bdd7
harim567 <harim0567@gmail.com>
2024-10-02 21:18:46 +0000

    updated

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from Models.Dall_e3 import display_Dall_e3
 
 load_dotenv()
 HUGGINGFACE_API_KEY = st.secrets['HUGGINGFACE_API_KEY']
-HUGGINGFACE_API_KEY1 = st.secrets['HUGGINGFACE_API_KEY1']
-OPEN_AI_API = st.secrets['OPEN_AI_API']
+#HUGGINGFACE_API_KEY1 = st.secrets['HUGGINGFACE_API_KEY1']
+#OPEN_AI_API = st.secrets['OPEN_AI_API']
 
 st.set_page_config(
         page_title="Generative Image",
@@ -23,7 +23,8 @@ st.set_page_config(
 with st.sidebar:
     # Create the options menu
     selected = option_menu(menu_title="Image-Gen Models",
-                           options=["Stable Diffusion XL","Brea v2","DreamShaper v7","Dall-e2","Anime Diffusion"],
+                         #  options=["Stable Diffusion XL","Brea v2","DreamShaper v7","Dall-e2","Anime Diffusion"],
+                           options=["Stable Diffusion XL"],
                            icons=["box", "box","box","box","box"],
                            menu_icon="boxes",
                            default_index=0
@@ -31,11 +32,11 @@ with st.sidebar:
     
 if selected == "Stable Diffusion XL":
     display_SDF(HUGGINGFACE_API_KEY)
-elif selected == "Brea v2":
-    display_Brea(HUGGINGFACE_API_KEY)
-elif selected == "DreamShaper v7":
-    display_DreamShaper_v7(HUGGINGFACE_API_KEY1)
-elif selected == "Anime Diffusion":
-    display_Anime_df(HUGGINGFACE_API_KEY1)
-elif selected == "Dall-e2":
-    display_Dall_e3(OPEN_AI_API)
+# elif selected == "Brea v2":
+#     display_Brea(HUGGINGFACE_API_KEY)
+# elif selected == "DreamShaper v7":
+#     display_DreamShaper_v7(HUGGINGFACE_API_KEY1)
+# elif selected == "Anime Diffusion":
+#     display_Anime_df(HUGGINGFACE_API_KEY1)
+# elif selected == "Dall-e2":
+#     display_Dall_e3(OPEN_AI_API)
