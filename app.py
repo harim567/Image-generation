4def4d0eedc4f5261d3cbd6c7726bc7cc3ec1ee1
harim567 <harim0567@gmail.com>
2024-10-02 21:23:55 +0000

    Enabled other LLM's

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from Models.Dall_e3 import display_Dall_e3
 
 load_dotenv()
 HUGGINGFACE_API_KEY = st.secrets['HUGGINGFACE_API_KEY']
-#HUGGINGFACE_API_KEY1 = st.secrets['HUGGINGFACE_API_KEY1']
+HUGGINGFACE_API_KEY1 = st.secrets['HUGGINGFACE_API_KEY']
 #OPEN_AI_API = st.secrets['OPEN_AI_API']
 
 st.set_page_config(
@@ -32,11 +32,11 @@ with st.sidebar:
     
 if selected == "Stable Diffusion XL":
     display_SDF(HUGGINGFACE_API_KEY)
-# elif selected == "Brea v2":
-#     display_Brea(HUGGINGFACE_API_KEY)
-# elif selected == "DreamShaper v7":
-#     display_DreamShaper_v7(HUGGINGFACE_API_KEY1)
-# elif selected == "Anime Diffusion":
-#     display_Anime_df(HUGGINGFACE_API_KEY1)
+elif selected == "Brea v2":
+    display_Brea(HUGGINGFACE_API_KEY)
+elif selected == "DreamShaper v7":
+    display_DreamShaper_v7(HUGGINGFACE_API_KEY1)
+elif selected == "Anime Diffusion":
+    display_Anime_df(HUGGINGFACE_API_KEY1)
 # elif selected == "Dall-e2":
 #     display_Dall_e3(OPEN_AI_API)
